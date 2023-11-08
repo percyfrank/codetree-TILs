f34f3a460bb1fa8594830c175e02e9c988cea594
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-11-08 22:01:13 +0900

    Title: 구간 중 최대 합, Time: 144ms, Memory: 26MB, Status: Passed - CodeTree

--- 231108/구간 중 최대 합/max-sum-of-subarray.py
+++ 231108/구간 중 최대 합/max-sum-of-subarray.py
@@ -3,6 +3,8 @@ nums = list(map(int,input().split()))
 
 ans = 0
 for i in range(n-k+1):
-    tmp = sum(nums[i:i+k])
+    tmp = 0
+    for j in range(i,i+k):
+        tmp += nums[j]
     ans = max(ans,tmp)
 print(ans)
