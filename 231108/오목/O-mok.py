d16935070de83d7fd41073cdf1192192b54e43dc
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-11-08 17:58:19 +0900

    Title: 오목, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 231108/오목/O-mok.py
+++ 231108/오목/O-mok.py
@@ -1,21 +1,33 @@
+import sys
+
 maps = [list(map(int,input().split())) for _ in range(19)]
 
-ans,x,y = 0,0,0
-for i in range(2,17):
-    for j in range(2,17):
-        if maps[i][j] != 0:
-            if maps[i][j-2] == maps[i][j-1] == maps[i][j] == maps[i][j+1] == maps[i][j+2]:
-                ans,x,y = maps[i][j],i+1,j+1
-            elif maps[i-2][j] == maps[i-1][j] == maps[i][j] == maps[i+1][j] == maps[i+2][j]:
-                ans,x,y = maps[i][j],i+1,j+1
-            elif maps[i-2][j-2] == maps[i-1][j-1] == maps[i][j] == maps[i+1][j+1] == maps[i+2][j+2]:
-                ans,x,y = maps[i][j],i+1,j+1
-            elif maps[i-2][j+2] == maps[i-1][j+1] == maps[i][j] == maps[i+1][j-1] == maps[i+2][j-2]:
-                ans,x,y = maps[i][j],i+1,j+1
-        
+def in_range(x, y):
+    return 0 <= x and x < 19 and 0 <= y and y < 19
 
-if ans != 0:
-    print(ans)
-    print(x,y)
-else:
-    print(ans)
+dxs = [0,1,1,1,0,-1,-1,-1]
+dys = [1,1,0,-1,-1,-1,0,1]
+
+for i in range(19):
+    for j in range(19):
+        if maps[i][j] == 0:
+            continue
+
+        for dx,dy in zip(dxs,dys):
+            x,y = i,j
+            cnt = 1
+            while True:
+                nx = x + dx
+                ny = y + dy
+                if not in_range(nx,ny) or maps[nx][ny] != maps[i][j]:
+                    break
+                cnt += 1
+                x = nx
+                y = ny
+
+                if cnt == 5:
+                    print(maps[i][j])
+                    print(i+2*dx+1,j+2*dy+1)
+                    sys.exit()
+
+print(0)
