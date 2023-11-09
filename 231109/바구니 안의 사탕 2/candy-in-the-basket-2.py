0ebad961f692f186495af71d275fc9d469eac997
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-11-09 23:52:42 +0900

    Title: 바구니 안의 사탕 2, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 231109/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 231109/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -2,19 +2,20 @@ n,k = tuple(map(int,input().split()))
 
 position = [0] * 101
 
+
+def in_range(idx):
+    return idx >=0 and idx < 101
+
 for _ in range(n):
     candy, pos = tuple(map(int,input().split()))
     position[pos] += candy
 
-
 ans = 0
-if k > 50:
-    ans = sum(position)
-else:
-    for i in range(k,101-k):
-        tmp = 0
-        for j in range(i-k,i+k+1):
+for i in range(101):
+    tmp = 0
+    for j in range(i-k,i+k+1):
+        if in_range(j):
             tmp += position[j]
-        ans = max(ans,tmp)
+    ans = max(ans,tmp)
 
 print(ans)
