08810b588670ec965137a1ef857d8ac330fc358f
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-11-13 21:17:40 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 5053ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

---
+++ 231113/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,18 @@
+n = int(input())
+a1,b1,c1 = tuple(map(int,input().split()))
+a2,b2,c2 = tuple(map(int,input().split()))
+
+result = []
+cnt = 0
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        for c in range(1,n+1):
+            if (abs(a-a1) <= 2 or abs(a-a1) >= n-2) and (abs(b-b1) <= 2 or abs(b-b1) >= n-2) and (abs(c-c1) <= 2 or abs(c-c1) >= n-2):
+                result.append((a,b,c))
+                cnt += 1    
+            if (a,b,c) not in result:
+                if (abs(a-a2) <= 2 or abs(a-a2) >= n-2) and (abs(b-b2) <= 2 or abs(b-b2) >= n-2) and (abs(c-c2) <= 2 or abs(c-c2) >= n-2):
+                    result.append((a,b,c))
+                    cnt += 1
+    
+print(cnt)
