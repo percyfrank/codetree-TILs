a060ee7374c0ca09693ceda80ed3dcc505506751
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-11-14 17:46:47 +0900

    Title: 삼각형 만들기, Time: 167ms, Memory: 28MB, Status: Passed - CodeTree

--- 231114/삼각형 만들기/create-triangle.py
+++ 231114/삼각형 만들기/create-triangle.py
@@ -5,20 +5,13 @@ points = [list(map(int,input().split())) for _ in range(n)]
 
 
 def get_area(x1,y1,x2,y2,x3,y3):
-    return abs((x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3)) / 2
+    return abs((x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3))
 
 ans = 0
 for (x1,y1),(x2,y2),(x3,y3) in combinations(points,3):
     if y1 == y2 == y3 or x1 == x2 == x3:
         continue
-    elif y1 == y2:
-        if x3 == x1 or x3 == x2:
-            ans = max(ans,get_area(x1,y1,x2,y2,x3,y3))
-    elif y2 == y3:
-        if x1 == x2 or x1 == x3:
-            ans = max(ans,get_area(x1,y1,x2,y2,x3,y3))
-    elif y1 == y3:
-        if x2 == x1 or x2 == x3:
-            ans = max(ans,get_area(x1,y1,x2,y2,x3,y3))
+    if (x1 == x2 or x1 == x3 or x2 == x3) and (y1 == y2 or y1 == y3 or y2 == y3):
+        ans = max(ans,get_area(x1,y1,x2,y2,x3,y3))
     
-print(int(ans*2))
+print(ans)
