5fe15d04c198b3a67439dc473b2534b87da4e6d0
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-11-14 17:00:39 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 231114/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 231114/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -1,10 +1,12 @@
 n = int(input())
 coords = [list(map(int,input().split())) for _ in range(n)]
 
+def dist(i,j):
+    return (coords[i][0]-coords[j][0])**2 + (coords[i][1]-coords[j][1])**2
+
 ans = float('inf')
 for i in range(n):
     for j in range(i+1,n):
-        tmp = (coords[i][0]-coords[j][0])**2 + (coords[i][1]-coords[j][1])**2
-        ans = min(ans,tmp)
+        ans = min(ans,dist(i,j))
     
 print(ans)
